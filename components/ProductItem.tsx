ca25eac936811357488ff102026cdfbb4c5fadc9
Gabriele Nakassima <gaby.naka@gmail.com>
2021-09-17 17:15:29 -0300

    memo, useMemo, useCallback

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,15 +1,26 @@
+import { memo } from 'react';
+
 interface ProductItemProps {
     product: {
         id: number;
         price: number;
         title: string;
-    }
+    };
+    onAddToWishlist: (id: number) => void;
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+export function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
     return (
         <div>
             {product.title} - <strong>{product.price}</strong>
+            
+            <button onClick={() => onAddToWishlist(product.id)}>
+                Add to wishlist
+            </button>
         </div>
     )
 }
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+});
