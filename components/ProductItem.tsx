a71516713b7e0293784da177313679f666eaf4ae
Gabriele Nakassima <gaby.naka@gmail.com>
2021-09-17 21:02:28 -0300

    Otimizando o código e performance

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,22 +1,41 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
+import dynamic from 'next/dynamic';
+
+import { AddProductWishlistProps } from './AddProductWishlist';
+
+const AddProductWishlist = dynamic<AddProductWishlistProps>(() => {
+    return import('./AddProductWishlist').then(mod => mod.AddProductWishlist)
+}, {
+    loading: () => <span>Carregando...</span>
+})
 
 interface ProductItemProps {
     product: {
         id: number;
         price: number;
         title: string;
+        priceFormatted: string;
     };
     onAddToWishlist: (id: number) => void;
 }
 
 export function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
+    const [isAddingToWishlist, setIsAddingToWishlist] = useState(false);
+
     return (
         <div>
-            {product.title} - <strong>{product.price}</strong>
-            
-            <button onClick={() => onAddToWishlist(product.id)}>
-                Add to wishlist
+            {product.title} - <strong>{product.priceFormatted}</strong>
+
+            <button onClick={() => setIsAddingToWishlist(true)}>
+                Adicionar aos favoritos
             </button>
+
+            {isAddingToWishlist && (
+                <AddProductWishlist
+                    onAddToWishlist={() => onAddToWishlist(product.id)}
+                    onRequestClose={() => setIsAddingToWishlist(false)}
+                />
+            )}
         </div>
     )
 }
